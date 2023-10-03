27be6a8f9a87d8e021cce81f54512d2980368ce3
Joebim <josey359@gmail.com>
2023-10-03 22:56:45 +0100

    [ADD] New UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+const Home = lazy(() => import('./global-components/Home'));
+import Loader from './global-components/Loader';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes>
-
-      </Routes>
+      <Suspense fallback={<Loader/>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Suspense>
     </>
   )
 }
---
+++ src/global-components/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="w-full h-[100vh] p-[30px] flex justify-center items-center bg-white">
+        <div className="h-full w-full bg-black rounded-[40px]"></div>
+    </div>
+    </>
+  )
+}
